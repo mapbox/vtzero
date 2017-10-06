2e5082e59ef5eda70e4ed56264cd6d98cf814eb2
Jochen Topf <jochen@topf.org>
2017-10-06 10:55:12 +0200

    Split up unit tests.

---
+++ test/t/test_property_map.cpp
@@ -0,0 +1,56 @@
+
+#include <vtzero/builder.hpp>
+
+#include <catch.hpp>
+
+#ifdef VTZERO_TEST_WITH_VARIANT
+# include <boost/variant.hpp>
+#endif
+
+#include <map>
+#include <unordered_map>
+
+TEST_CASE("property map") {
+    vtzero::tile_builder tile;
+    vtzero::layer_builder layer_points{tile, "points"};
+    {
+        vtzero::point_feature_builder feature{layer_points, 1};
+        feature.add_points(1);
+        feature.set_point(10, 10);
+        feature.add_property("foo", "bar");
+        feature.add_property("x", "y");
+        feature.add_property("abc", "def");
+    }
+
+    std::string data = tile.serialize();
+
+    vtzero::vector_tile vt{data};
+    REQUIRE(vt.size() == 1);
+    auto layer = *vt.begin();
+    REQUIRE(layer.size() == 1);
+    auto feature = *layer.begin();
+
+    REQUIRE(feature.size() == 3);
+
+#ifdef VTZERO_TEST_WITH_VARIANT
+    SECTION("std::map") {
+        using prop_map_type = std::map<std::string, variant_type>;
+        auto map = vtzero::create_properties_map<prop_map_type>(feature);
+
+        REQUIRE(map.size() == 3);
+        REQUIRE(boost::get<std::string>(map["foo"]) == "bar");
+        REQUIRE(boost::get<std::string>(map["x"]) == "y");
+        REQUIRE(boost::get<std::string>(map["abc"]) == "def");
+    }
+    SECTION("std::unordered_map") {
+        using prop_map_type = std::unordered_map<std::string, variant_type>;
+        auto map = vtzero::create_properties_map<prop_map_type>(feature);
+
+        REQUIRE(map.size() == 3);
+        REQUIRE(boost::get<std::string>(map["foo"]) == "bar");
+        REQUIRE(boost::get<std::string>(map["x"]) == "y");
+        REQUIRE(boost::get<std::string>(map["abc"]) == "def");
+    }
+#endif
+}
+
