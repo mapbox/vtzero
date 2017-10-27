5c1a85a8f4bece7c701e2b663ef79bb0f51f8217
Jochen Topf <jochen@topf.org>
2017-10-27 17:22:31 +0200

    Rename function parameter for consistency.

--- examples/utils.hpp
+++ examples/utils.hpp
@@ -5,7 +5,7 @@
 
 std::string read_file(const std::string& filename);
 
-void write_data_to_file(const std::string& data, const std::string& filename);
+void write_data_to_file(const std::string& buffer, const std::string& filename);
 
 vtzero::layer get_layer(const vtzero::vector_tile& tile, const std::string& layer_name_or_num);
 
