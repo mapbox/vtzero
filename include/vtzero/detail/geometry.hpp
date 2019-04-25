7271065956f8d23a46b6d11eb698c03dda18f1b7
Jochen Topf <jochen@topf.org>
2019-04-25 13:33:38 +0200

    Sort includes.

--- include/vtzero/detail/geometry.hpp
+++ include/vtzero/detail/geometry.hpp
@@ -16,11 +16,11 @@ documentation.
  * @brief Contains classes and functions related to geometry handling.
  */
 
-#include "attributes.hpp"
-#include "util.hpp"
 #include "../exception.hpp"
 #include "../point.hpp"
 #include "../types.hpp"
+#include "attributes.hpp"
+#include "util.hpp"
 
 #include <protozero/pbf_reader.hpp>
 
