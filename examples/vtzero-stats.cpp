5f386e0beb8f5a1d6bc4f068fd67e91fe753f044
Jochen Topf <jochen@topf.org>
2023-08-09 10:15:14 +0200

    Add layer raw size and geometries raw size to stats, use CSV format

--- examples/vtzero-stats.cpp
+++ examples/vtzero-stats.cpp
@@ -14,12 +14,24 @@
 #include <stdexcept>
 #include <string>
 
+static std::size_t geometries_size(const vtzero::layer& layer) {
+    std::size_t sum = 0;
+
+    layer.for_each_feature([&sum](vtzero::feature&& feature) {
+        sum += feature.geometry().data().size();
+        return true;
+    });
+
+    return sum;
+}
+
 int main(int argc, char* argv[]) {
     if (argc != 2) {
         std::cerr << "Usage: " << argv[0] << " TILE\n";
         return 1;
     }
 
+    std::cout << "layer,num_features,raw_size,raw_geometries_size,key_table_size,value_table_size\n";
     try {
         std::string input_file{argv[1]};
         const auto data = read_file(input_file);
@@ -28,9 +40,11 @@ int main(int argc, char* argv[]) {
 
         while (const auto layer = tile.next_layer()) {
             std::cout.write(layer.name().data(), static_cast<std::streamsize>(layer.name().size()));
-            std::cout << ' '
-                      << layer.num_features() << ' '
-                      << layer.key_table().size() << ' '
+            std::cout << ','
+                      << layer.num_features() << ','
+                      << layer.data().size() << ','
+                      << geometries_size(layer) << ','
+                      << layer.key_table().size() << ','
                       << layer.value_table().size() << '\n';
         }
     } catch (const std::exception& e) {
